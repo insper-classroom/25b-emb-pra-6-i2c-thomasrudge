ac2f6cb1dc93ba15d275a14174fdc2f3675e15bd
thomasrudge <thomasfr@al.insper.edu.br>
2025-10-15 20:27:02 -0300

    final

--- main/main.c
+++ main/main.c
@@ -9,12 +9,21 @@
 #include "hardware/gpio.h"
 #include "hardware/i2c.h"
 #include "mpu6050.h"
+#include <math.h>
 
-#include <Fusion.h>
+#include "Fusion.h"
+#define SAMPLE_PERIOD (0.01f) // replace this with actual sample period
 
 const int MPU_ADDRESS = 0x68;
 const int I2C_SDA_GPIO = 4;
 const int I2C_SCL_GPIO = 5;
+
+QueueHandle_t xQueuePos;
+
+typedef struct btn {
+    int axis;
+    int val;
+} btn;
 
 static void mpu6050_reset() {
     // Two byte reset. First byte register, second byte data
@@ -58,7 +67,10 @@ static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
     *temp = buffer[0] << 8 | buffer[1];
 }
 
+
+
 void mpu6050_task(void *p) {
+    // configuracao do I2C
     i2c_init(i2c_default, 400 * 1000);
     gpio_set_function(I2C_SDA_GPIO, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL_GPIO, GPIO_FUNC_I2C);
@@ -68,47 +80,102 @@ void mpu6050_task(void *p) {
     mpu6050_reset();
     int16_t acceleration[3], gyro[3], temp;
 
-    gpio_init(15);
-    gpio_init(16);
-    gpio_set_dir(15, GPIO_OUT);
-    gpio_set_dir(16, GPIO_IN);
-    gpio_pull_up(16);
-    gpio_put(15,0);
-    int dataArray[90] = {0};
-    int16_t pDataXYZ[3] = {0};
+    FusionAhrs ahrs;
+    FusionAhrsInitialise(&ahrs);
+    btn newbtn;
+
+    int acc;
+
     while(1) {
-        // printf("%d,%d, %d,\n", acceleration[0], acceleration[1], acceleration[2]);
-        // printf("Gyro. X = %d, Y = %d, Z = %d\n", gyro[0], gyro[1], gyro[2]);
-        // printf("Temp. = %f\n", (temp / 340.0) + 36.53);
+        mpu6050_read_raw(acceleration, gyro, &temp);
+      FusionVector gyroscope = {
+          .axis.x = gyro[0] / 131.0f, // Conversão para graus/s
+          .axis.y = gyro[1] / 131.0f,
+          .axis.z = gyro[2] / 131.0f,
+      };
 
-        int a = 0;
-        for(int i = 0; i < 30; i++){
-            mpu6050_read_raw(pDataXYZ, gyro, &temp);
-            a = i*3;
-            dataArray[a] = pDataXYZ[0]; 			// x
-            dataArray[a + 1] = pDataXYZ[1]; 	// y
-            dataArray[a + 2] = pDataXYZ[2]; 	// z
-            
-            printf(" %d,", dataArray[a]);
-            printf(" %d,", dataArray[a + 1]);
-            printf(" %d,", dataArray[a + 2]);
-            // HAL_Delay(100);
-            vTaskDelay(pdMS_TO_TICKS(100));
+      FusionVector accelerometer = {
+          .axis.x = acceleration[0] / 16384.0f, // Conversão para g
+          .axis.y = acceleration[1] / 16384.0f,
+          .axis.z = acceleration[2] / 16384.0f,
+      };      
+  
+      FusionAhrsUpdateNoMagnetometer(&ahrs, gyroscope, accelerometer, SAMPLE_PERIOD);
+  
+      const FusionEuler euler = FusionQuaternionToEuler(FusionAhrsGetQuaternion(&ahrs));
+  
+     // printf("Roll %0.1f, Pitch %0.1f, Yaw %0.1f\n", euler.angle.roll, euler.angle.pitch, euler.angle.yaw);
 
-        }
-        printf(" \n");
-        gpio_put(15,0);
-        while(gpio_get(16) == 1);
-        gpio_put(15,1);
+      //printf("Acc. X = %d, Y = %d, Z = %d\n", acceleration[0], acceleration[1], acceleration[2]);
+
+      
+    if (euler.angle.pitch < 20.0  && euler.angle.pitch > -20.0){
+        ;
+    }
+    else{
+        newbtn.axis = 1;
+        newbtn.val = (int) euler.angle.pitch;
+        //printf("%06d \n", newbtn.val);
+        xQueueSend(xQueuePos, &newbtn, 0);
     }
 
+    if (euler.angle.roll < 20.0  && euler.angle.roll > -20.0){
+        ;
+    }
+    else{
+        newbtn.axis = 0;
+        newbtn.val = (int) euler.angle.roll;
+       xQueueSend(xQueuePos, &newbtn, 0);
+    }
+      
+    
 
+    acc = sqrt((acceleration[0]*acceleration[0]) + (acceleration[1]*acceleration[1]) + (acceleration[2]*acceleration[2])) - 15000;
+
+      if (acc < 15000  && acc > -15000){
+        ;
+      } 
+      else{
+        newbtn.axis = 2;
+        newbtn.val = 0;
+        xQueueSend(xQueuePos, &newbtn, 0);
+
+
+      }
+      vTaskDelay(pdMS_TO_TICKS(10));
+
+
+    }
+}
+
+void uart_task(void *p){
+    btn xya;
+
+    while (1){
+
+          if (xQueueReceive(xQueuePos, &xya, pdMS_TO_TICKS(200)) == pdTRUE){
+
+            
+            uart_putc_raw(uart0, xya.axis);
+            uart_putc_raw(uart0, xya.val);
+            uart_putc_raw(uart0, xya.val >> 8);
+
+            uart_putc_raw(uart0, -1);
+            
+            
+          }
+
+    }
 }
 
 int main() {
     stdio_init_all();
 
-    xTaskCreate(mpu6050_task, "mpu6050_Task 1", 8192, NULL, 1, NULL);
+    xQueuePos = xQueueCreate(3, sizeof(btn));
+
+    xTaskCreate(mpu6050_task, "mpu6050_Task 1", 18192, NULL, 1, NULL);
+
+   xTaskCreate(uart_task, "mpu4323446050_Task 1", 10192, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
